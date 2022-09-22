965c1793a183f87da58347868a42abbc482c9b86
johnli <johnli@johns-mbp.dhcp.wustl.edu>
2022-09-22 13:40:36 -0500

    code

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -11,7 +11,15 @@ public class Sieve {
 		int[] arrayAllNumbers = new int[n+1];
 		for (int i = 0; i <= n; i++) {
 			arrayAllNumbers[i] = i;
-			System.out.println(arrayAllNumbers[i]);
+			//System.out.println(arrayAllNumbers[i]);
+		}
+		for (int k = 0; k < arrayAllNumbers.length; k++) {
+			int[] arrayTemp = new int[arrayAllNumbers.length];
+			if (arrayAllNumbers[k]%2 != 0 || arrayAllNumbers[k] == 2) {
+				arrayTemp[k]=arrayAllNumbers[k];
+				System.out.println(arrayTemp[k]);
+			}
+			
 		}
 
 	}
