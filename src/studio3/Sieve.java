18f3fd4dc9e6396ade249d2226ada85a2d45b95c
johnli <johnli@johns-mbp.dhcp.wustl.edu>
2022-09-22 15:01:25 -0500

    finished

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -7,21 +7,37 @@ public class Sieve {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Please input a integer number n.");
-		int n = in.nextInt();
-		int[] arrayAllNumbers = new int[n+1];
-		for (int i = 0; i <= n; i++) {
-			arrayAllNumbers[i] = i;
-			//System.out.println(arrayAllNumbers[i]);
+		int n = in.nextInt()+1;
+		Boolean[] arrayAllNumbers = new Boolean[n];
+		for (int h = 0; h < n; h ++) {
+			arrayAllNumbers[h] = true;
 		}
-		for (int k = 0; k < arrayAllNumbers.length; k++) {
-			int[] arrayTemp = new int[arrayAllNumbers.length];
-			if (arrayAllNumbers[k]%2 != 0 || arrayAllNumbers[k] == 2) {
-				arrayTemp[k]=arrayAllNumbers[k];
-				System.out.println(arrayTemp[k]);
+		
+		for (int i = 2; i <= Math.sqrt(n); i++) {
+			if (arrayAllNumbers[i] == true) {
+				for (int j = (int) Math.pow(i, 2); j <= n; j = j + i) {
+					arrayAllNumbers[j] = false;
+				}
+			}	
+		}
+		int l = 0;
+		for (int e = 2; e < n; e++) {
+			if (arrayAllNumbers[e] == true) {
+				l++;
 			}
-			
+		}
+		int [] newList = new int[l+1];
+		int q =0;
+		for (int y = 2; y < n; y++) {
+			if (arrayAllNumbers[y] == true) {
+				
+				newList[q] = y;
+				q++;
+			}
+		}
+		for (int h = 0; h<l; h ++) {
+			System.out.print(newList[h]+ ", ");
 		}
 
 	}
-
 }
