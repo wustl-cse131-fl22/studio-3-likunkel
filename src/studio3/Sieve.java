e10306f380c10cb02a7571e6f0723eb9931db986
johnli <johnli@johns-mbp.dhcp.wustl.edu>
2022-09-22 13:20:36 -0500

    new

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -7,7 +7,12 @@ public class Sieve {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.println("Please input a integer number n.");
-		
+		int n = in.nextInt();
+		int[] arrayAllNumbers = new int[n+1];
+		for (int i = 0; i <= n; i++) {
+			arrayAllNumbers[i] = i;
+			System.out.println(arrayAllNumbers[i]);
+		}
 
 	}
 
