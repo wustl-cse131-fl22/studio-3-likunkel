8ab6001f54ee5a6b214e416367c4d75873803481
johnli <johnli@johns-mbp.dhcp.wustl.edu>
2022-09-22 13:09:50 -0500

    set up

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -1,9 +1,13 @@
 package studio3;
 
+import java.util.Scanner;
+
 public class Sieve {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please input a integer number n.");
+		
 
 	}
 
